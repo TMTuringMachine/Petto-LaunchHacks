547f65bfffa5a4f98540e828ac3ebe72f8a0670e
AryanShinde <aryan.shinde20@vit.edu>
2022-06-04 15:08:08 +0530

    react app initialized

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env" });
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+app.use(cors());
+
+//using middleware to parse json data
+app.use(express.json());
+
+require("./db/conn");
+app.use("/", require("./routes/UserRoutes"));
+app.use("/request", require("./routes/RequestRoutes"));
+
+app.listen(port, () => {
+  console.log(`App listening on port http://localhost:${port}`);
+});
