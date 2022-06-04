829f2f9157a55d810ea99039769e8dbcf9c9f13f
AryanShinde <aryan.shinde20@vit.edu>
2022-06-04 21:33:16 +0530

    providers added

---
+++ client/src/providers/jwtProvider.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import useAuth from "../hooks/useAuth";
+// ----------------------------------------------------------------------
+
+export default function JwtProvider({ children }) {
+  const dispatch = useDispatch();
+  const { initializeAuth } = useAuth();
+
+  useEffect(() => {
+    initializeAuth();
+  }, []);
+
+  return <>{children}</>;
+}
